8bc444f214445271eec0142057cf364a28b383a1
silvanos <ericoderito@gmail.com>
2022-08-18 17:17:35 +0300

    feat(css): add tailwind support

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,15 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+		adapter: adapter()
 	}
 };
 
